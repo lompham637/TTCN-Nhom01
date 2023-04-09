8a66c746c59b54ee7cf28bdd466c5b16ced2f3fb
lompham637 <vukhahuylinh0@gmail.com>
2023-04-09 14:20:27 +0700

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+import NavBar from "./components/Nav";
+import Banner from "./components/Banner";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Section />
+      <Footer />
+     
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+function Footer() {
+    return ( 
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright © Your Website 2023</p></div>
+        </footer>
+     );
+}
+
+export default Footer;
