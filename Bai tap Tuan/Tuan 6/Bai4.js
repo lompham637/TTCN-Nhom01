595aac4a6bd063de76f55608a29bba6632257db4
MangVu <130735668+MangVu@users.noreply.github.com>
2023-04-23 10:01:45 +0700

    Delete Bai4.js

--- Bai tap Tuan/Tuan 6/Bai4.js
+++
@@ -1,9 +0,0 @@
-var elements = document.getElementsByClassName('elements');
-
-// In số lượng phần tử được lấy
-console.log('Tìm thấy ' + elements.length + ' phần tử.');
-
-// In tất cả các phần tử
-for (var i = 0; i < elements.length; i++) {
-  console.log(elements[i]);
-}
