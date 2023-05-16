b6d7592e37a28536a2bff234935739897a4e9269
lompham637 <vukhahuylinh0@gmail.com>
2023-05-16 16:50:40 +0700

    Cap nhat phan Sidebar va cart

--- Fashion-shop/Fashion-shop/src/components/Product.js
+++ Fashion-shop/Fashion-shop/src/components/Product.js
@@ -6,9 +6,17 @@ import { Link } from 'react-router-dom';
 //import icon
 import {BsPlus, BsEyeFill} from 'react-icons/bs';
 
+
+//import cart context
+import { CartContext } from '../contexts/CartContext';
+
 const Product = ({ product }) => {
+
+  const {addToCart} = useContext(CartContext);
+
   //destructor product
   const {id, image, category, title, price} = product;
+
   return <div>
     <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
         <div className='w-full h-full flex justify-center items-center'>
@@ -23,7 +31,7 @@ const Product = ({ product }) => {
           
           {/* Button */}
           <div className='absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all'>
-            <button>
+            <button onClick={() => addToCart(product, id)}>
               <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
                 <BsPlus className='text-3xl'/>
               </div>
